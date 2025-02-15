db699c71268b0635a8a6bf31c91e4ee1f15d0ac1
Karl-Mitx <kescobarc14@miumg.edu.gt>
2025-02-15 10:23:48 -0600

    Juego de adivinanzas

--- main.cpp
+++ main.cpp
@@ -3,8 +3,21 @@
 using namespace std;
 
 int main() {
+    //Inicio de los ejercicios
+    //Juego de adivinanzas de un numero
+
+    cout <<"Adivine el numero del 1 al 100";
+    cout <<endl;
+
+    int randomNumber;
+    int a;
+    cin >> a;
+    randomNumber = (rand() % (100-1) + 1);
+    cout << "El numero que usted tenia que adivinar era: " << randomNumber;
+    cout << endl;
 
 
 
 
+ return 0;
 }
